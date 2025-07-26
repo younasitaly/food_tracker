c4b7db0fa98d291c2b4864783ac48ca2ab185cdc
younasitaly <younasitaly@gmail.com>
2025-07-27 01:59:54 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('food-tracker-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
